8b11dd650ae7b86fa0945c6ad0ad3782cde192b9
udaykiranvtu <vtu28218@veltech.edu.in>
2026-09-18 12:03:36 +0530

    Create ManacherAlgorithmForFindingLongestPalindromicSubstring2.java

---
+++ week6/ManacherAlgorithmForFindingLongestPalindromicSubstring2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // Palindromes can be of odd length (centered at i) or even length (centered between i and i+1)
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            
+            // If we found a longer palindrome, update our start and end indices
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        
+        return s.substring(start, end + 1);
+    }
+    
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // Length of the palindrome found
+        return right - left - 1;
+    }
+}
