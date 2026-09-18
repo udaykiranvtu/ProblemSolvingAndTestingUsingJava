36d507fbad4175bf5eb5caaea75872f565a4d4f9
udaykiranvtu <vtu28218@veltech.edu.in>
2026-09-18 11:43:27 +0530

    Create AdvancedStringTechniques.java

---
+++ week5/AdvancedStringTechniques.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> seen = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (seen.contains(s.charAt(right))) {
+                seen.remove(s.charAt(left));
+                left++;
+            }
+            seen.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
