536671997e4268f5bc51190959dd335035358d9a
udaykiranvtu <vtu28218@veltech.edu.in>
2026-09-18 11:51:53 +0530

    Create StringHandlingBasics.java

---
+++ week5/StringHandlingBasics.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int myAtoi(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        int i = 0, n = s.length();
+        // Skip leading whitespaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // Handle optional sign
+        int sign = 1;
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        long result = 0; // use long to detect overflow
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+
+            // Clamp to int range
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+
+        return (int) (sign * result);
+    }
+}
